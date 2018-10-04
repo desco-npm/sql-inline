e5d53362ff10ee1d9391e11dabee9daf649dd4dd
Rafael Dias <contato@diasrafael.com.br>
2018-10-04 06:59:44 -0300

    m

---
+++ index.js
@@ -0,0 +1,23 @@
+const replaceall = require('replaceall')
+const getQuotesContent = require('@desco/get-quotes-content')
+
+const sqlInline = sql => {
+    let newSql = sql.split(`\n`).join(' ').trim()
+    const quoted = getQuotesContent(newSql)
+
+    quoted.map(q => {
+        newSql = newSql.replace(q, '[ToReplace]')
+    })
+
+    while (newSql.indexOf('  ') !== -1) {
+        newSql = replaceall('  ', ' ', newSql)
+    }
+
+    quoted.map(q => {
+        newSql = newSql.replace('[ToReplace]', q)
+    })
+
+    return newSql
+}
+
+module.exports = sqlInline
